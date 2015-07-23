d2b39676961b1fab57061dc95fc5172a2e2b5763
root <ingoklein0508@gmail.com>
2015-07-23 10:03:19 +0200

    child node

--- watir_child.rb
+++ watir_child.rb
@@ -1,8 +1,4 @@
-def child
-  ## xpath with child nodes
-  if swatch_tag.xpath('.//img').present?
-    color_info[:image_url] = swatch_tag.xpath('.//img/@src').text
-  else
-    color_info[:image_url] = image_urls[0]
-  end
-end
+require 'nokogiri'
+swatch_tag = doc.xpath('//div[contains(@class, "container")]')
+swatch_tag.xpath('.//img').present?
+    
